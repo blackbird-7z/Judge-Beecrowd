0d95d09b6be5b7cae93f9f9ea7dfcf6fb23216bd
hyago-dejesus <hyago@mha.com.br>
2026-01-04 08:23:38 -0300

    Primeiros exercicios

---
+++ Iniciante/BEE1002/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double n = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double pi5 = Math.Round(Math.PI, 5); // Pegar apenas 5 casas decimais após o ponto.
+        double area = pi5 * n * n;
+
+        Console.WriteLine("A=" + area.ToString("F4", CultureInfo.InvariantCulture));
+    }
+}
