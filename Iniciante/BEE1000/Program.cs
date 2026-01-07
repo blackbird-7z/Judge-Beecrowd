3b11825de23b48368d2e47e62a11f1db8ea769ad
hyago-dejesus <hyago@mha.com.br>
2026-01-07 19:32:44 -0300

    Alterando estrutura do repo

--- Iniciante/BEE1000/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System; // Sempre adicionar manualmente, caso contrário o site não aceita.
-
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World!");
-    }
-}
