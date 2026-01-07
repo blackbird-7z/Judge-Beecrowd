2575f0847ccf178a1c32b51a37a51e1e41038c50
hyago-dejesus <hyago@mha.com.br>
2026-01-07 05:53:50 -0300

    BEE1007-8

---
+++ Iniciante/BEE1008/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int NUMBER = int.Parse(Console.ReadLine());
+        int HOURS = int.Parse(Console.ReadLine());
+        double VALUE = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double SALARY = VALUE * HOURS;
+
+        Console.WriteLine("NUMBER = " + NUMBER);
+        Console.WriteLine("SALARY = U$ " + SALARY.ToString("F2", CultureInfo.InvariantCulture));
+
+    }
+
+}
