342b096367bfc23b067cb43bc1747c52614bc741
hyago-dejesus <hyago@mha.com.br>
2026-01-08 05:45:09 -0300

    BEE1009

---
+++ BEE1009/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string Nome = Console.ReadLine();
+        double SalarioFixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double VendasMes = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double Total = SalarioFixo + VendasMes * 0.15;
+
+        Console.WriteLine("TOTAL = R$ " + Total.ToString("F2", CultureInfo.InvariantCulture));
+
+    }
+
+}
