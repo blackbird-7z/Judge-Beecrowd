7d4a183d748d166dfd4d4d6a3a10ba578d0878b2
hyago-dejesus <hyago@mha.com.br>
2026-01-12 09:21:37 -0300

    BEE1010

---
+++ Iniciante/BEE1010/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        // Configura o ponto como separador padrão neste programa/thread.
+        CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+
+        string[] A = Console.ReadLine().Split(' ');
+        string[] B = Console.ReadLine().Split(' ');
+
+        double[] vetA = new double[A.Length];
+        double[] vetB = new double[B.Length];
+
+
+        for (int i = 0; i < A.Length; i++)
+        {
+            vetA[i] = double.Parse(A[i]);
+        }
+
+        for (int i = 0; i < B.Length; i++)
+        {
+            vetB[i] = double.Parse(B[i]);
+        }
+
+        double Total = vetA[1] * vetA[2] + vetB[1] * vetB[2];
+
+
+        Console.WriteLine("VALOR A PAGAR: R$ " + Total.ToString("F2"));
+
+    }
+
+}
